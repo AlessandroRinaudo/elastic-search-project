deb381a3e08b6f73f2203f6fe7b0299c87a9bcb0
Melimo94400 <irles.kevin@gmail.com>
2021-10-22 10:10:41 +0200

    add connection logstash

--- app/main.py
+++ app/main.py
@@ -9,9 +9,37 @@ import textract
 import time
 import os
 
+import logging
+import logstash
+import sys
+
+# Bah je fait des choses qui va faire le viens en faite
+host = 'logs-container'
+test_logger = logging.getLogger('python-logstash-logger')
+test_logger.setLevel(logging.INFO)
+test_logger.addHandler(logstash.LogstashHandler(host, 5959, version=1))
+
+# Et puis la je fait genre qu'il y a des choses a dire
+extra = {
+    'test_string': 'python version: ' + repr(sys.version_info),
+    'test_boolean': True,
+    'test_dict': {'a': 1, 'b': 'c'},
+    'test_float': 1.23,
+    'test_integer': 123,
+    'test_list': [1, 2, '3'],
+}
+
 app = FastAPI()
 es = Elasticsearch([{'host': 'es-container', 'port': 9200}])
 
+@app.get("/")
+async def test():
+    # et la je genere des logs en fait non ?
+    test_logger.error('python-logstash: test logstash error message.')
+    test_logger.info('python-logstash: test logstash info message.')
+    test_logger.warning('python-logstash: test logstash warning message.')
+    test_logger.info('python-logstash: test extra fields', extra=extra)
+    return { "test": 1}
 
 @app.post("/upload_pdf")
 async def upload_file(files: List[UploadFile] = File(...)):
